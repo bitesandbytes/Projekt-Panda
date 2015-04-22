dbbb1c1f0c6a6b404d490b0c3604d86eb2b138ca
J S Suhas <cs13b056@smail.iitm.ac.in>
2015-04-23 01:10:16 +0530

    Changed FileControlPacket class.

--- ServerSideApplication/src/common/FileControlPacket.java
+++ ServerSideApplication/src/common/FileControlPacket.java
@@ -1,8 +1,18 @@
 package common;
 
-public class FileControlPacket
+import java.io.Serializable;
+
+public class FileControlPacket implements Serializable
 {
+	private static final long serialVersionUID = 1465289083771282932L;
 	public boolean isServer;
 	public String payload;
 	public boolean isIP;
+	
+	public FileControlPacket(boolean isServer, String payload, boolean isIP)
+	{
+		this.isIP = isIP;
+		this.isServer = isServer;
+		this.payload = payload;
+	}
 }
--- ServerSideApplication/src/coreServerThreads/FileRequestHandlerThread.java
+++ ServerSideApplication/src/coreServerThreads/FileRequestHandlerThread.java
@@ -2,6 +2,7 @@ package coreServerThreads;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 import common.FileControlPacket;
@@ -9,11 +10,13 @@ import coreServer.UserMap;
 
 public class FileRequestHandlerThread extends Thread
 {
+	private static int destPort = 4500;
 	private Socket srcSocket;
 	private UserMap userMap;
 	private ObjectInputStream srcOIS;
 	private FileControlPacket controlPacket;
 	private int retryCount = 3;
+	private ObjectOutputStream srcOOS;
 
 	public FileRequestHandlerThread(UserMap map, Socket srcSocket)
 	{
@@ -52,5 +55,64 @@ public class FileRequestHandlerThread extends Thread
 			}
 		}
 		retryCount = 3;
+		String destIP = userMap.getCurrentIP(controlPacket.payload);
+		if(destIP == null)
+		{
+			writeSrc(new FileControlPacket(true, "0.0.0.0", true));
+			return;
+		}
+		else
+		{
+			//TODO:: write controlPacket to client at destIP, destport as 4500.
+			try
+			{
+				Socket destSocket = new Socket(destIP, destPort);
+			}
+			catch (IOException e)
+			{
+				System.out.println();
+			}
+			
+			//TODO:: Receive acknowledgement.
+			//TODO:: Write a file control packet to client with open socket.
+		}
+	}
+	
+	private void writeSrc(FileControlPacket packet)
+	{
+		try
+		{
+			srcOOS = new ObjectOutputStream(srcSocket.getOutputStream());
+		}
+		catch (IOException e)
+		{
+			System.out.println("Unable to get OOS for src. Dropping request.");
+			return;
+		}
+		controlPacket.isServer = true;
+		controlPacket.payload = "0.0.0.0";
+		controlPacket.isIP = true;
+		
+		while (retryCount > 0)
+		{
+			try
+			{
+				srcOOS.writeObject(controlPacket);
+				srcSocket.close();
+				return;
+			}
+			catch (IOException e)
+			{
+				retryCount--;
+				if(retryCount == 0)
+				{
+					System.out.println("Unable to write to src. Dropping request.");
+					return;
+				}
+				continue;
+			}
+		}
+		return;
+		
 	}
 }
