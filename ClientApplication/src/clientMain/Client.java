7d5381838df28a3e216c30e677977f90e4fcebae
J S Suhas <cs13b056@smail.iitm.ac.in>
2015-04-23 19:16:45 +0530

    Changed Global class

--- ClientApplication/src/clientMain/Client.java
+++ ClientApplication/src/clientMain/Client.java
@@ -1,26 +1,32 @@
 package clientMain;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import common.LoginRequest;
 import coreClientThreads.LoginRequestThread;
+
 /*
- *  Note that LoginRequest is Equivalent to a User Object. It is used throughout to access current Client's Nick
+ * Note that LoginRequest is Equivalent to a User Object. It is used throughout
+ * to access current Client's Nick
  */
-public class Client {
+public class Client
+{
 	public static Boolean isLoggedIn = false;
 	public static Boolean isConnected = false;
 	public static LoginRequest user;
 	public static ArrayList<LoginRequest> friends = new ArrayList<LoginRequest>();
-	private static Scanner sc; 
-	public static void main(String [] args){
+	private static Scanner sc;
+
+	public static void main(String[] args)
+	{
 		sc = new Scanner(System.in);
 		int loginNumber;
 		System.out.println("Welcome. Choose 0 for sign up. 1 for login");
 		loginNumber = sc.nextInt();
 		String nick;
 		String password;
-		if(loginNumber == 0)
+		if (loginNumber == 0)
 			System.out.println("Welcome to User Sign Up Interface");
 		else
 			System.out.println("Welcome to User Login Interface");
@@ -29,7 +35,7 @@ public class Client {
 		System.out.println("Enter your password: ");
 		password = sc.next();
 		user = new LoginRequest(nick, password);
-		if(loginNumber == 0)
+		if (loginNumber == 0)
 			user.isSignup = true;
 		else
 			user.isSignup = false;
--- ClientApplication/src/coreClient/Global.java
+++ ClientApplication/src/coreClient/Global.java
@@ -4,12 +4,11 @@ public class Global
 {
 	public static String serverIP = "10.42.0.27";
 	public static String defaultFileSavePath = "/home/sauce/Documents";
-	public static int serverFileRequestPort = 4500;
-	public static int serverMsgPort = 2500;
-	public static int serverLoginRequestPort = 3500;
+	public static int serverFileRequestPort = 4400;
+	public static int serverMsgPort = 2400;
+	public static int serverLoginRequestPort = 3400;
 	public static int localMsgReceivePort = 2500;
 	public static int clientFilePort = 4500;
-	public static int fileControlPort = 4400;
 
 	public static void Log(Object o)
 	{
