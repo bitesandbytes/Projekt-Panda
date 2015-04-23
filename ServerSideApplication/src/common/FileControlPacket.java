88dacacc4e109b3724d6435fe4b81c64b0f05603
J S Suhas <cs13b056@smail.iitm.ac.in>
2015-04-23 15:24:43 +0530

    Added fileName field in FileControlPacket class.

--- ServerSideApplication/src/common/FileControlPacket.java
+++ ServerSideApplication/src/common/FileControlPacket.java
@@ -7,6 +7,7 @@ public class FileControlPacket implements Serializable
 	private static final long serialVersionUID = 1465289083771282932L;
 	public boolean isServer;
 	public String payload;
+	public String fileName;
 	public boolean isIP;
 	
 	public FileControlPacket(boolean isServer, String payload, boolean isIP)
@@ -14,5 +15,6 @@ public class FileControlPacket implements Serializable
 		this.isIP = isIP;
 		this.isServer = isServer;
 		this.payload = payload;
+		this.fileName = null;
 	}
 }
